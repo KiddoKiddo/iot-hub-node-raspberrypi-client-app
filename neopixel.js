18b0650643c6ccbb925f2e8e5339d7e42cfa74f7
Truong Chau Thy <truong_chau_thy@artc.a-star.edu.sg>
2018-07-24 14:52:43 +0800

    Debug

--- neopixel.js
+++ neopixel.js
@@ -1,7 +1,25 @@
 'use strict';
 
 var strip = require("./ws2812controller/strip.js");
+var xmas = require("./ws2812controller/animations/xmas.js");
+var fade = require("./ws2812controller/animations/fade.js");
 var rainbow = require("./ws2812controller/animations/rainbow.js");
+var control = require("./ws2812controller/animations/control.js");
+
+/*
+  control
+    Stop:
+  fade
+    GoFade2({ Color1: '66CCFF', Color2: '66CCFF' })
+    FadeSpeed({ speed: 1-100 })
+  rainbow:
+    GoRainbow()
+    RainbowSpeedd({ speed: 1-100 })
+  xmas:
+    GoXmas1();
+    GoXmasIterate();
+
+*/
 
 var args = {};
 rainbow.GoRainbow(args, strip);
